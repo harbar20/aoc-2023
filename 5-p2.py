3b31fd800a27acd51249db507ccea1356937909f
Harshith Iyer <harbar20@Harshiths-MBP-2.lan>
2023-12-09 17:07:49 -0500

    day 5 finally done

--- 5-p2.py
+++ 5-p2.py
@@ -1,62 +1,85 @@
+import pprint
+
 obj = {}
 
 with open("5.txt") as f:
-	everything = f.read()
-	everything = everything.split("\n\n")
+    everything = f.read()
+    everything = everything.split("\n\n")
 
 # Original seeds
 oldSeeds = everything[0].split(": ")[1].strip().split(" ")
 oldSeeds = [int(i) for i in oldSeeds]
 seeds = {}
 for i in range(0, len(oldSeeds), 2):
-    seeds[oldSeeds[i]] = {
-        "srcRangeStart": oldSeeds[i],
-        "rangeLen": oldSeeds[i+1]
-    }
+    seeds[oldSeeds[i]] = (oldSeeds[i], oldSeeds[i + 1])
 
 # Adding everything to the object
 obj["seed"] = seeds
 keys = []
 for i in range(1, len(everything)):
-	mapSection = everything[i]
-	parts = mapSection.split("\n")
+    mapSection = everything[i]
+    parts = mapSection.split("\n")
 
-	title = parts[0]
-	titleParts = title.split(" ")[0].split("-")
-	first = titleParts[0]
-	last = titleParts[2]
-	
-	keys.append(last)
+    title = parts[0]
+    titleParts = title.split(" ")[0].split("-")
+    first = titleParts[0]
+    last = titleParts[2]
 
-	obj[last] = {}
-	numbers = parts[1:]
-	
-	for line in numbers:
-		lineParts = line.strip().split(" ")
-		destRangeStart = int(lineParts[0])
-		srcRangeStart = int(lineParts[1])
-		rangeLen = int(lineParts[2])
+    keys.append(last)
 
-		obj[last][srcRangeStart] = {
-			"destRangeStart": destRangeStart,
-			"srcRangeStart": srcRangeStart,
-			"rangeLen": rangeLen
-		}
-  
+    obj[last] = {}
+    numbers = parts[1:]
+
+    for line in numbers:
+        lineParts = line.strip().split(" ")
+        destRangeStart = int(lineParts[0])
+        srcRangeStart = int(lineParts[1])
+        rangeLen = int(lineParts[2])
+
+        obj[last][srcRangeStart] = {
+            "destRangeStart": destRangeStart,
+            "srcRangeStart": srcRangeStart,
+            "rangeLen": rangeLen,
+        }
+
 newObj = {}
-originalSubKeys = seeds
-# Keeping only the soils that are values with the seeds as keys
+previousSubKeys = list(seeds.values())
 for key in keys:
-    sortedGivenSubKeys = sorted(obj[key].keys())
-    
     newObj[key] = {}
+
+    j = 0
+    while j < len(previousSubKeys):
+        subkey = previousSubKeys[j]
+        subkeyStart = subkey[0]
+        subkeyRange = subkey[1]
+
+        found = False
+        for currentSubKey in obj[key].keys():
+            currentSubValue = obj[key][currentSubKey]
+
+            if subkeyStart in range(currentSubKey, currentSubKey + currentSubValue["rangeLen"]):
+                found = True
+                
+                if subkeyStart+subkeyRange in range(currentSubKey, currentSubKey + currentSubValue["rangeLen"]):
+                    newObj[key][(subkeyStart, subkeyRange)] = (
+                        currentSubValue["destRangeStart"],
+                        subkeyRange,
+                    )
+                else:
+                    subSubRange = subkeyStart+currentSubValue["rangeLen"]
+                    newObj[key][(subkeyStart, subSubRange)] = (
+                        currentSubValue["destRangeStart"],
+                        subSubRange,
+                    )
+                    previousSubKeys.insert(j+1, (subkeyStart+subSubRange+1, subSubRange+subkeyRange+1))
+                
+                break
+        
+        if not found:
+            newObj[key][(subkeyStart, subkeyRange)] = (subkeyStart, subkeyRange)
+        
+        j += 1
     
-    for subkey in originalSubKeys:
-        subkeyKey = subkey["srcRangeStart"]
-        subkeyRange = subkey["rangeLen"]
-        
-        if subkeyKey < sortedGivenSubKeys[0]:
-            newObj[key][(0)] = subkey
-            continue
-        
-        
+    previousSubKeys = list(newObj[key].values())
+
+print(min([i[0] for i in list(newObj["location"].values())]))
